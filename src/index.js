d655fee87ec21658cd2aeb317a38e644ca11790b
Ricky Sophin <sophinricky@gmail.com>
2022-07-07 13:18:18 -0400

    initial import

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+const App = () => {
+    return (
+        <div>
+            <p>Placeholder</p>
+        </div>
+    )
+}
+
+const appElement = document.getElementById("app");
+ReactDOM.render(<App />, appElement)
