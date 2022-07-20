99f1a9ac1ca7384920be15c47ef6cee35768eab7
Ricky Sophin <sophinricky@gmail.com>
2022-07-19 21:46:37 -0400

    new changes

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>Wecome to Stranger's Things!</h1>
+            <p></p>
+            <button>View Profile</button>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>Messages to Me:</p>
+            <p>Messages from Me:</p>
+        </div>
+    )
+}
+
+export default Profile
